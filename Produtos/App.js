b23f99912290ebceccaa1863570941928df885d0
Antônio Matheus Follone Graça <matheusfollone_trabalho@outlook.com>
2022-05-10 13:30:50 -0300

    commit

--- Produtos/App.js
+++ Produtos/App.js
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // importando telas
 
-import Home from
+import Home from './src/pages/Home'
 
 const Stack= createNativeStackNavigator();
 
