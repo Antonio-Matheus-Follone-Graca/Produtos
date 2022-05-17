1f98f98fe7e726d72b14a801e9113b837a5f95ad
Antônio Matheus Follone Graça <matheusfollone_trabalho@outlook.com>
2022-05-17 12:05:48 -0300

    commit

--- Produtos/App.js
+++ Produtos/App.js
@@ -19,7 +19,7 @@ const Stack= createNativeStackNavigator();
 export default function App(){
   return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="CadastrarUsuario">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Home"
           component={Home}
           options={{
