5d00f81c5166060fdc656c454e5163b710b03172
Antônio Matheus Follone Graça <matheusfollone_trabalho@outlook.com>
2022-05-18 20:56:12 -0300

    Update Home.js

--- Produtos/src/pages/Home.js
+++ Produtos/src/pages/Home.js
@@ -71,7 +71,52 @@ export default function Home({navigation,route}){
   return(
     <View style={styles.container}>
       {/* ao tirar a flatlist some o erro não faco a menor ideia do por quê*/ }
-      
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={produtos}
+        keyExtractor={item=>item.id}
+        renderItem={  ({item})=>{
+          return(<View style={styles.ProdutosFlatlist}> 
+              <View style={styles.StyleProdutos}> 
+                  <View style={styles.icones}> 
+                    <TouchableOpacity onPress={()=>{
+                    deletar(item.id)
+                  }}>
+                  
+                  
+                  <MaterialCommunityIcons name="trash-can" color={'#f92e6a'} size={25}/> 
+                 
+                  </TouchableOpacity>
+
+                  <TouchableOpacity onPress={()=>{
+                    navigation.navigate('Alterar',{
+                      idProduto:item.id,
+                      descricaoNova:item.descricao,
+                      nomeProdutoNovo:item.nome,
+                      precoNovo:item.preco
+                      
+                    })
+                   
+                  }}>
+                  
+                  
+                  <MaterialCommunityIcons name="pencil" color={'#f92e6a'} size={25}/>
+                   
+                  </TouchableOpacity>
+                  
+                  </View>
+                  <View style={styles.StyleInfo}> 
+                    <Text style={styles.textoDados}> {item.nome}</Text>
+                    <View style={styles.iconePreco}>  
+                      <FontAwesomeIcon icon={faBrazilianRealSign}  size={20} color="#7CFC00"/>
+                      <Text style={{fontSize:20,fontWeight:'bold'}}> {item.preco} </Text>
+                    </View>
+                  </View>
+              
+              </View>
+            </View>)
+        } }
+      />
       <TouchableOpacity style={styles.ButaoAdd} onPress={()=>{
         navigation.navigate("Cadastrar")
       }}>
