ecd4ca7a633f27f8dff5c3d8e9cc88d986c01a3f
Antônio Matheus Follone Graça <matheusfollone_trabalho@outlook.com>
2022-05-18 17:58:06 -0300

    Update Home.js

--- Produtos/src/pages/Home.js
+++ Produtos/src/pages/Home.js
@@ -19,6 +19,7 @@ import {faBrazilianRealSign} from '@fortawesome/free-solid-svg-icons'
 
 export default function Home({navigation,route}){
   const[produtos,setProdutos]=useState([])
+  const[loading,setLoading]=useState(true)
 
   const deletar=(idProduto)=>{
     //Alert.alert("produto deletado com sucesso","id:"+idProduto)
@@ -62,60 +63,23 @@ export default function Home({navigation,route}){
       });
  
       // preenchendo hook dos produtos com a lista do select
+     
       setProdutos(lista)
- 
-    })
+      console.log(produtos)
+      console.log(lista)
+     
+      
+      })
+     setLoading(false)
    },[])
 
    
   return(<View style={styles.container}>
-      {/* ao tirar a flatlist some o erro*/ }
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        data={produtos}
-        keyExtractor={item=>item.id}
-        renderItem={  ({item})=>{
-          return(<View style={styles.ProdutosFlatlist}> 
-              <View style={styles.StyleProdutos}> 
-                  <View style={styles.icones}> 
-                    <TouchableOpacity onPress={()=>{
-                    deletar(item.id)
-                  }}>
-                  
-                  
-                  <MaterialCommunityIcons name="trash-can" color={'#f92e6a'} size={25}/> 
-                 
-                  </TouchableOpacity>
-
-                  <TouchableOpacity onPress={()=>{
-                    navigation.navigate('Alterar',{
-                      idProduto:item.id,
-                      descricaoNova:item.descricao,
-                      nomeProdutoNovo:item.nome,
-                      precoNovo:item.preco
-                      
-                    })
-                   
-                  }}>
-                  
-                  
-                  <MaterialCommunityIcons name="pencil" color={'#f92e6a'} size={25}/>
-                   
-                  </TouchableOpacity>
-                  
-                  </View>
-                  <View style={styles.StyleInfo}> 
-                    <Text style={styles.textoDados}> {item.nome}</Text>
-                    <View style={styles.iconePreco}>  
-                      <FontAwesomeIcon icon={faBrazilianRealSign}  size={20} color="#7CFC00"/>
-                      <Text style={{fontSize:20,fontWeight:'bold'}}> {item.preco} </Text>
-                    </View>
-                  </View>
-              
-              </View>
-            </View>)
-        } }
-      />
+      {!loading &&
+        <flatList/>
+      }
+     
+    
       <TouchableOpacity style={styles.ButaoAdd} onPress={()=>{
         navigation.navigate("Cadastrar")
       }}>
